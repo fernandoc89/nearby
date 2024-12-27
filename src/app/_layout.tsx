1254cb89d51eef9c58bf4f0bfc257ab4a7cdfd2c
fernandoc89 <fernandocardoso@live.com>
2024-12-27 17:05:24 -0300

    feature (1): adding styles and api integration

---
+++ src/app/_layout.tsx
@@ -0,0 +1,36 @@
+import { colors } from "@/styles/theme";
+import { Stack } from "expo-router";
+import { GestureHandlerRootView } from "react-native-gesture-handler";
+
+import {
+  Rubik_400Regular,
+  Rubik_500Medium,
+  Rubik_600SemiBold,
+  Rubik_700Bold,
+  useFonts,
+} from "@expo-google-fonts/rubik";
+import { Loading } from "./components/loading";
+
+export default function Layout() {
+  const [fontsLoaded] = useFonts({
+    Rubik_600SemiBold,
+    Rubik_400Regular,
+    Rubik_500Medium,
+    Rubik_700Bold,
+  });
+
+  if (!fontsLoaded) {
+    return <Loading />;
+  }
+
+  return (
+    <GestureHandlerRootView style={{ flex: 1 }}>
+      <Stack
+        screenOptions={{
+          headerShown: false,
+          contentStyle: { backgroundColor: colors.gray[100] },
+        }}
+      />
+    </GestureHandlerRootView>
+  );
+}
